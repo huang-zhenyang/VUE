e691e2057aa0fd09120efd3bde7e6875b1c498f5
huang-zhenyang <2658047092@qq.com>
2024-12-05 15:28:23 +0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'; // 引入 Element Plus 的样式
 
-createApp(App).mount('#app')
+// 创建 Vue 应用实例
+const app = createApp(App);
+
+// 使用 Element Plus
+app.use(ElementPlus);
+
+// 挂载 Vue 应用
+app.mount('#app');
